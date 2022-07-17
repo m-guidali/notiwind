eb61a51648fadeb98276547c5739efba0b6547db
m-guidali <57319159+m-guidali@users.noreply.github.com>
2022-07-17 11:08:00 +0200

    Allow the option to never time out
    By setting timeout = -1, the notification will never timeout

--- src/Notification.vue
+++ src/Notification.vue
@@ -76,10 +76,12 @@ export default {
       const DEFAULT_TIMEOUT = 3000
 
       this.notifications.push(notification)
-
-      this.timeouts[notification.id] = setTimeout(() => {
-        this.remove(notification.id)
-      }, timeout || DEFAULT_TIMEOUT)
+      
+      if(timeout !== -1) {
+        this.timeouts[notification.id] = setTimeout(() => {
+          this.remove(notification.id)
+        }, timeout || DEFAULT_TIMEOUT)
+      }
     },
     close(id) {
       this.$emit('close')
